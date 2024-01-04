ca299bbfeea0ce25ad560098ff50f04edff72b8a
Dopezapha <officialnwaneridaniel@gmail.com>
2024-01-04 14:24:59 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,12 @@
 from sys import argv
 
 if __name__ == "__main__":
-argc = len(argv) - 1
+	argc = len(argv) - 1
 
-if argc == 0:
-print("0 arguments.")
-else:
-print("{} {}:".format(argc, "argument" if argc == 1 else "arguments"))
+	if argc == 0:
+		print("0 arguments.")
+	else:
+		print("{} {}:".format(argc, "argument" if argc == 1 else "arguments"))
 
-for i in range(1, argc + 1):
-print("{}: {}".format(i, argv[i]))
+	for i in range(1, argc + 1):
+		print("{}: {}".format(i, argv[i]))
