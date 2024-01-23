b2abee5047d5eb24a946a1ce1e2be1d589cd4c00
Dopezapha <officialnwaneridaniel@gmail.com>
2024-01-23 10:19:22 -0800

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,6 +8,7 @@ class Square:
     def __init__(self, size=0):
         """ Initializes the new instance to a smaller class """
         
+        try:
             if type(size) is not int:
                 raise TypeError("size must be an integer")
             if size < 0:
