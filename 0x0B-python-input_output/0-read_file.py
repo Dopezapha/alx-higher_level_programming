8dd1984afd7db400d054caae41db9a4f24a86d16
Dopezapha <officialnwaneridaniel@gmail.com>
2024-02-07 04:59:05 -0800

    a function that reads a text file (UTF8) and prints it to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,9 +3,6 @@
 def read_file(filename=""):
     """
     Reads the content of a file and prints it."""
-    if filename:
         with open(filename, encoding="utf-8") as file:
             for line in file:
                 print(line, end="")
-    else:
-        print("Please provide a valid filename.")
