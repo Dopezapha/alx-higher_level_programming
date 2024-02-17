c408b745149ccfde397def7649df64e6ea39150c
Dopezapha <officialnwaneridaniel@gmail.com>
2024-02-17 04:08:29 -0800

    the class Rectangle that inherits from Base

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,20 +8,7 @@ class Rectangle(Base):
     """Represent a rectangle."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize a new Rectangle.
-
-        Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
-            x (int): The x coordinate of the new Rectangle.
-            y (int): The y coordinate of the new Rectangle.
-            id (int): The identity of the new Rectangle.
-        Raises:
-            TypeError: If either of width or height is not an int.
-            ValueError: If either of width or height <= 0.
-            TypeError: If either of x or y is not an int.
-            ValueError: If either of x or y < 0.
-        """
+        """Initialize a new Rectangle."""
         self.width = width
         self.height = height
         self.x = x
@@ -58,7 +45,7 @@ class Rectangle(Base):
     def x(self):
         """Set/get the x coordinate of the Rectangle."""
         return self.__x
- 
+
     @x.setter
     def x(self, value):
         if type(value) != int:
