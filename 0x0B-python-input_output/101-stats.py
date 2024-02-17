72bf6720084441f99d6500fff444b63b592080d3
Dopezapha <officialnwaneridaniel@gmail.com>
2024-02-17 01:55:27 -0800

    a script that reads stdin line by line and computes metrics

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,20 +1,29 @@
+#!/usr/bin/python3
 import datetime
 import random
 import sys
 from time import sleep
 
+"""Simulated HTTP request log generator"""
 for i in range(10000):
+    """Introduce random delay to mimic real-world request intervals"""
     sleep(random.random())
+
+    """Generate and write a log entry to stdout"""
     sys.stdout.write(
         "{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n"
         .format(
+            """Generate a random IP address"""
             random.randint(1, 255),
             random.randint(1, 255),
             random.randint(1, 255),
             random.randint(1, 255),
+            """Record the current timestamp"""
             datetime.datetime.now(),
+            """Choose a random HTTP status code"""
             random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+            """Generate a random response size"""
             random.randint(1, 1024)
         )
     )
-    sys.stdout.flush()
+    sys.stdout.flush
