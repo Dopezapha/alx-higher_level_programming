67cf7dfc63aed0f777124065ade0451dbde59550
Dopezapha <officialnwaneridaniel@gmail.com>
2024-02-17 06:25:56 -0800

    a class BaseGeometry based on 5-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A class of BaseGeometry"""
+
+
+class BaseGeometry:
+    """Declaration of BaseGeometry"""
+
+    def area(self):
+        raise Exception(area() is not implemented)
