8cf7d79e22cb799d9caa4dde1c971668c68b06bf
Dopezapha <officialnwaneridaniel>
2024-04-09 00:05:46 -0700

    Javascript Warmup

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,2 @@
 #!/usr/bin/node
-// Check if both arguments are passed
-if (process.argv[2] !== undefined && process.argv[3] !== undefined) {
-  console.log(`${process.argv[2]} is ${process.argv[3]}`);
-} else {
-  console.log('Please provide two arguments');
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-const num = parseInt(process.argv[2]);
-
-if (!isNaN(num)) {
-  console.log(`My number: ${num}`);
-} else {
+if (isNaN(parseInt(process.argv[2])) === true) {
   console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
