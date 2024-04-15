db65bffabcd2b15b62e4e18c7e1de9ddf8d986a1
Dopezapha <officialnwaneridaniel>
2024-04-15 04:32:16 -0700

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
 import MySQLdb
 import sys
-import os
 """a script that lists all states from the database hbtn_0e_0_usa"""
 
 
 if __name__ == "__main__":
     """Connect to MySQL server"""
-    db = MySQLdb.connect(host="localhost", port=3306, user="root",
+    conn = MySQLdb.connect(host="localhost", port=3306, user="root",
                          passwd="root", db="hbtn_0e_0_usa")
 
     """Create a cursor object"""
-    cursor = db.cursor()
+    cur = conn.cursor()
 
     """Execute the query to fetch all states"""
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-    """clear the console"""
-    os.system('cls' if os.name == 'nt' else 'clear')
+    """query states"""
+    states = cur.fetchall()
 
     """Print the states"""
-    for state in cursor.fetchall():
+    for state in states:
         print(state)
 
     """Close cursor and database connection"""
-    cursor.close()
-    db.close()
+    cur.close()
+    conn.close()
