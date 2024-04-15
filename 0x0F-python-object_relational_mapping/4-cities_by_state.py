ae4e19ba85cc9251f4ba93347343d754cbc67c9c
Dopezapha <officialnwaneridaniel>
2024-04-15 10:27:39 -0700

    a script that lists all cities from the database

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -13,9 +13,10 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     """Execute SQL query"""
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities
-                LEFT JOIN states ON states.id = cities.state_id
-                ORDER BY cities.id ASC")
+    cur.execute("SELECT cities.id, cities.name, states.name\
+                 FROM cities LEFT JOIN states\
+                 ON states.id = cities.state_id\
+                 ORDER BY cities.id ASC")
 
     """Fetch all rows"""
     cities = cur.fetchall()
