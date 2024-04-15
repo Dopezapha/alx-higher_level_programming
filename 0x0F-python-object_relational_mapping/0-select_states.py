56fa77cea48a48d363aa6cf74e88367dd3db7f2d
Dopezapha <officialnwaneridaniel>
2024-04-15 05:00:36 -0700

    a script that lists all states from the database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == "__main__":
     """Connect to MySQL server"""
     conn = MySQLdb.connect(host="localhost", port=3306, user="root",
-                         passwd="root", db="hbtn_0e_0_usa")
+                           passwd="root", db="hbtn_0e_0_usa")
 
     """Create a cursor object"""
     cur = conn.cursor()
@@ -18,9 +18,15 @@ if __name__ == "__main__":
     """query states"""
     states = cur.fetchall()
 
+    """Initialize a set to keep track of printed states"""
+    printed_state_names = set()
+
     """Print the states"""
     for state in states:
-        print(state)
+        state_name = state[1]
+        if state_name not in printed_state_names:
+            print(state)
+            printed_state_names.add(state_name)
 
     """Close cursor and database connection"""
     cur.close()
