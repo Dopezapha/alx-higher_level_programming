a125ce4d7290599f94e1b800242c20dcb72d8ef3
Dopezapha <officialnwaneridaniel@gmail.com>
2024-04-25 14:42:52 +0100

    a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# A Bash script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-
+# A Bash script that takes in a URL, sends a request to that URL and gets the size.
 [ $# -ne 1 ] && { echo "Usage: $0 <URL>"; exit 1; }
 size=$(curl -s "$1" | wc -c); [ -z "$size" ] && 
 	echo "Unable to retrieve size of the response body." || echo "$size"
