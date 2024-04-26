3f4a0e511370a97b5eee82fa6a6e11a2c03f27f4
Dopezapha <officialnwaneridaniel@gmail.com>
2024-04-26 12:59:13 +0100

    a Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,5 +3,5 @@
 import urllib.request
 
 if __name__ == "__main__":
-with urllib.request.urlopen(https://alx-intranet.hbtn.io/status) as response:
-    html = response.read()
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        html = response.read()
