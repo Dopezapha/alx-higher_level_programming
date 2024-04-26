92584737f10350654579dbcb851ef92b94ef5621
Dopezapha <officialnwaneridaniel@gmail.com>
2024-04-26 11:49:43 +0100

    Python - Network #0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# A Bash script that takes in a URL, sends a request to that URL and gets the size.
-[ $# -ne 1 ] && { echo "Usage: $0 <URL>"; exit 1; }
-size=$(curl -s "$1" | wc -c); [ -z "$size" ] && 
-	echo "Unable to retrieve size of the response body." || echo "$size"
+# A script that takes in a URL, sends a request to that URL
+curl -s "$1" | wc -c
