d1dddb7a9441b86827387b5cba506666ae77db46
root <root@DESKTOP-MGUKQKN>
2024-05-23 20:10:33 +1000

    a script that reads and prints the content of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+"use strict";
+
+const fs = require('fs');
+
+function readFile(filePath) {
+	try {
+		const content = fs.readFileSync(filePath, 'utf-8');
+		console.log(content);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+if (require.main === module) {
+	if (process.argv.length !== 3) {
+		console.error("Usage: ./script.js <file_path>");
+	} else {
+		const filePath = process.argv[2];
+		readFile(filePath);
+	}
+}
+
+module.exports = readFile;
